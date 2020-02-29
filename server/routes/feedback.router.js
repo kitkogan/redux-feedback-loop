d295211aeec9e70fa870874e4f302e3ece5100a3
Kit Kogan <kkogan.dev@gmail.com>
2020-02-29 10:33:19 -0600

    set up server side get route

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+// Using a array of data on the server, we will eventually
+// move this back into the database.
+const feedback = require('../modules/pool');
+
+router.get('/', (req, res) => {
+    pool.query(`SELECT * FROM feedback;`)
+    .then(result => {res.send(result.rows)})
+    .catch(err => {
+        console.log('GET req error', err);
+        res.sendStatus(500);
+    })
+})
+
+module.exports = router;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+//imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//Redux
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+
+const feedbackReducer = (state = [], action) => {
+    if(action.type === 'SET_FEEDBACK') {
+        return action.payload
+    }
+    return state;
+}
+
+//Store holds all info for feedback app
+const storeInstance = createStore(
+    combineReducers({
+       feedbackReducer,
+    }),
+    applyMiddleware(logger),
+);
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+
+registerServiceWorker.unregister();
