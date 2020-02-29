eba317c1d270fa20c05101235faca38017a2e05b
Kit Kogan <kkogan.dev@gmail.com>
2020-02-29 12:01:30 -0600

    set up Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import './App.css';
+import Header from './../Header/Header.js';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
+        <Header />
         <br/>
       </div>
     );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <h1 className="App-title">Daily Feedback!</h1>
+                <h4><i>...And don't you forget it!</i></h4>
+            </header>
+        )
+    }
+}
+
+export default Header;
