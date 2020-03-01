7804bea7f3a69ee30a7f18fc6bde53a3e2eb825c
Kit Kogan <kkogan.dev@gmail.com>
2020-03-01 11:23:51 -0600

    add back button for support page

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -26,6 +26,17 @@ class Support extends Component {
         }
     }
 
+    handleBackClick = () => {
+        const under = this.state.understand;
+        const action = {type: 'UNDERSTAND_UPDATE', payload: under};
+        if(under === 0) {
+            alert('this field cannot be left blank');
+        } else {
+            this.props.dispatch(action);
+            this.props.history.push('/Understanding');
+        }
+    }
+
     //user selected rating will be updated in local state
     handleSelectedSupport = (ratingScore) => {
         this.setState({
@@ -43,7 +54,10 @@ class Support extends Component {
                 <h3>How well were you supported today?</h3>
                 <label className="dailyRating">Please select a number from 1-5 that corresponds with how supported you felt today</label>
                 <Rating feedback="Support" onChange={this.handleSelectedSupport} />
+                <button className="handleBackButton" onClick={this.handleBackClick}>BACK!</button>
+
                 <button className="handleNextButton" onClick={this.handleClickNext}>NEXT!</button>
+
             </div>
         )
     }
